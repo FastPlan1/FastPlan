255db505cc2091533b5ef3723d12057144c1c028
FastPlan1 <fastplan@outlook.fr>
2025-08-07 18:51:31 +0200

    Force update vehicleRoutes.js

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
 const Vehicle = require("../models/Vehicle");
 const multer = require("multer");
 const path = require("path");
